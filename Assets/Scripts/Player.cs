083f6e1a63fc52b37aa7e403c9bf95643df567d0
Eric Mesa <ericsbinaryworld@gmail.com>
2020-01-22 20:53:19 -0500

    Some rotation
    But it currently will try to re-orient itself to facing up if you stop pushing left/right

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // config params
+    [Header("Player")] // to organize in Unity
+    [SerializeField] float moveSpeed = 2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var forwardMovement = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var rotationMovement = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newRotation = (transform.rotation.z + rotationMovement);
+        transform.Rotate(0, 0, newRotation, Space.Self);
+    }
+}
