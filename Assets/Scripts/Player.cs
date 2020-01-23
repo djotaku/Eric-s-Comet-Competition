cb3c8524466121a3be26fecea3436c7182208643
Eric Mesa <ericsbinaryworld@gmail.com>
2020-01-22 21:02:45 -0500

    Fixed Rotation
    And also made it match the arrow keys expected.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     // config params
     [Header("Player")] // to organize in Unity
     [SerializeField] float moveSpeed = 2f;
+    [SerializeField] float rotationSpeed = 100f;
 
 
     // Start is called before the first frame update
@@ -24,8 +25,7 @@ public class Player : MonoBehaviour
     private void Move()
     {
         var forwardMovement = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var rotationMovement = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        var newRotation = (transform.rotation.z + rotationMovement);
-        transform.Rotate(0, 0, newRotation, Space.Self);
+        var rotationMovement = Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed;
+        transform.Rotate(0, 0, -rotationMovement, Space.Self);
     }
 }
